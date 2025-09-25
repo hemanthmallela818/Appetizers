13968a5ebcc0f559e75020adea0f59e6178318b6
MALLELA HEMANTH KUMAR <hemanthmallela818@gmail.com>
2025-09-25 14:42:46 +0530

    Add files via upload

---
+++ Appetizers_App/Appetizers_App/Appetizers_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Appetizers_AppApp.swift
+//  Appetizers_App
+//
+//  Created by Nxtwaves Mac mini-4 on 25/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Appetizers_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
